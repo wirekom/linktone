faaf7c689c8cbb5b8ce38d93fd08bbec4b7e9c22
ivanbackout <ivanbackout@gmail.com>
2014-01-07 16:19:02 +0700

    Squashed commit of the following:
    commit 3434ca6ae1d4dd108417554e476c5d11a540eb1d
    Author: Widodo Pangestu <wid.pangestu@gmail.com>
    Date:   Tue Jan 7 11:52:50 2014 +0700
    
        create user registration form and view user profile
        Signed-off-by: Widodo Pangestu <wid.pangestu@gmail.com>
    
    commit 0fa1507acf78de33791ac0a9bf6c4dfb5945278b
    Author: Widodo Pangestu <wid.pangestu@gmail.com>
    Date:   Tue Jan 7 10:50:20 2014 +0700
    
        add username and email validation
        fix retypepassword
        Signed-off-by: Widodo Pangestu <wid.pangestu@gmail.com>
    
    commit 4669cce4162b0652ca7d2d6013e542aa82d13211
    Merge: 489b2f9 c4303d7
    Author: Widodo Pangestu <wid.pangestu@gmail.com>
    Date:   Tue Jan 7 10:10:24 2014 +0700
    
        Merge branch 'master' of https://github.com/wirekom/linktone
    
    commit 489b2f9944bbbe663ef28d20bbf52df5fe9104bf
    Author: Widodo Pangestu <wid.pangestu@gmail.com>
    Date:   Tue Jan 7 10:09:49 2014 +0700
    
        move controller to backend
    
        Signed-off-by: Widodo Pangestu <wid.pangestu@gmail.com>

--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -1,6 +1,6 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="container">
+<div class="inner">
     <?php echo $content; ?>
 </div>
 <?php $this->endContent(); ?>
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -74,130 +74,144 @@
         </script>
     </head>
     <body>
-        <div class="container">
-            <!-- HEADER START -->	
-            <header class="clearfix">
-                <div class="top">
-                    <div class="logo">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/tpl/icon/logo.png" width="76" height="58" alt="logo movie bay" />
-                    </div>
-                    <!-- Menu Start -->
-                    <nav>
-                        <ul>
-                            <li>
-                                <a href="channel.html" title="Channel">CHANNEL</a>
-                                <ul>
-                                    <li><a href="#">RCTI</a></li>
-                                    <li><a href="#">MNC TV</a></li>
-                                    <li><a href="#">Global TV</a></li>	
-                                    <li><a href="#">Sindo TV</a></li>
-                                    <li><a href="#">MNC News</a></li>
-                                    <li><a href="#">MNC Business</a></li>
-                                    <li><a href="#">MNC Sport</a></li>
-                                    <li><a href="#">MNC Lifestyle</a></li>
-                                    <li><a href="#">MNC Kids</a></li>
-                                    <li><a href="#">MNC Fashion</a></li>
-                                    <li><a href="#">MNC Drama</a></li>
-                                    <li><a href="#">MNC Muslim</a></li>
-                                    <li><a href="#">MNC Food</a></li>
-                                    <li><a href="#">MNC Travel</a></li>
-                                    <li><a href="#">MNC Shop</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="movie.html" title="Movie">MOVIE</a>
-                                <ul>
-                                    <li><a href="#">Disney</a></li>
-                                    <li><a href="#">Western</a></li>
-                                    <li><a href="#">Chinese</a></li>
-                                    <li><a href="#">Indonesian</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="tv.html" title="TV Show">TV SERIES</a>
-                                <ul>
-                                    <li><a href="#">Disney</a></li>
-                                    <li><a href="#">Western</a></li>
-                                    <li><a href="#">Chinese</a></li>
-                                    <li><a href="#">MNC TV Movies</a></li>
-                                    <li><a href="#">MNC Drama Series</a></li>
-                                    <li><a href="#">MNC Comedy Series</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="kods.html" title="TV Show">KIDS</a>
-                                <ul>
-                                    <li><a href="#">Animated Series</a></li>
-                                    <li><a href="#">Animated Movie</a></li>
-                                    <li><a href="#">Family Movie</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="clip.html" title="Video Clip">CLIP</a>
-                                <ul>
-                                    <li><a href="#">Seputar Indonesia Clips</a></li>
-                                    <li><a href="#">Intens Clips</a></li>
-                                    <li><a href="#">Silet Clips</a></li>
-                                    <li><a href="#">Go Spot Clips</a></li>
-                                    <li><a href="#">Obsesi Clips</a></li>
-                                    <li><a href="#">Fokus Selebriti Clips</a></li>
-                                    <li><a href="#">Dahsyat Clips</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                    <!-- Menu END -->
-                    <div class="search">
-                        <form>
-                            <input type="text" />
-                            <input type="submit" value="" />
-                        </form>
-                    </div>
+		<!-- HEADER START -->	
+		<div class="header clearfix">
+			<div class="container">
+				<div class="top">
+					<div class="logo">
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>">
+							<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/tpl/icon/logo.png" width="76" height="58" alt="logo movie bay" />
+						</a>
+					</div>
+					<!-- Menu Start -->
+					<nav>
+						<ul class="menu">
+							<li>
+								<a href="channel.html" title="Channel">CHANNEL</a>
+								<ul>
+									<li><a href="#">RCTI</a></li>
+									<li><a href="#">MNC TV</a></li>
+									<li><a href="#">Global TV</a></li>	
+									<li><a href="#">Sindo TV</a></li>
+									<li><a href="#">MNC News</a></li>
+									<li><a href="#">MNC Business</a></li>
+									<li><a href="#">MNC Sport</a></li>
+									<li><a href="#">MNC Lifestyle</a></li>
+									<li><a href="#">MNC Kids</a></li>
+									<li><a href="#">MNC Fashion</a></li>
+									<li><a href="#">MNC Drama</a></li>
+									<li><a href="#">MNC Muslim</a></li>
+									<li><a href="#">MNC Food</a></li>
+									<li><a href="#">MNC Travel</a></li>
+									<li><a href="#">MNC Shop</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="movie.html" title="Movie">MOVIE</a>
+								<ul>
+									<li><a href="#">Disney</a></li>
+									<li><a href="#">Western</a></li>
+									<li><a href="#">Chinese</a></li>
+									<li><a href="#">Indonesian</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="tv.html" title="TV Show">TV SERIES</a>
+								<ul>
+									<li><a href="#">Disney</a></li>
+									<li><a href="#">Western</a></li>
+									<li><a href="#">Chinese</a></li>
+									<li><a href="#">MNC TV Movies</a></li>
+									<li><a href="#">MNC Drama Series</a></li>
+									<li><a href="#">MNC Comedy Series</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="kods.html" title="TV Show">KIDS</a>
+								<ul>
+									<li><a href="#">Animated Series</a></li>
+									<li><a href="#">Animated Movie</a></li>
+									<li><a href="#">Family Movie</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="clip.html" title="Video Clip">CLIP</a>
+								<ul>
+									<li><a href="#">Seputar Indonesia Clips</a></li>
+									<li><a href="#">Intens Clips</a></li>
+									<li><a href="#">Silet Clips</a></li>
+									<li><a href="#">Go Spot Clips</a></li>
+									<li><a href="#">Obsesi Clips</a></li>
+									<li><a href="#">Fokus Selebriti Clips</a></li>
+									<li><a href="#">Dahsyat Clips</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+					<!-- Menu END -->
+					<div class="search">
+						<form>
+							<input type="text" />
+							<input type="submit" value="" />
+						</form>
+					</div>
 
-                    <!-- USER START -->
-                    <div class="user l">
-                        <ul>
-                            <li><a href="#" title="Upgrade previllage">UPGRADE</a></li>
-                            <li><a href="#" title="Nama USer">Si Fulan (0)</a>
-                                <ul>
-                                    <li><a href="#" title="Akun">Akun</a></li>
-                                    <li><a href="#" title="Pesan">Pesan <span class="r">(0)</span></a></li>
-                                    <li><a href="#" title="favorit">Favorit</a></li>
-                                    <li><a href="#" title="Petunjuk">Petunjuk</a></li>
-                                    <li><a href="#" title="Keluar">Keluar</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#" title="Avatar"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/tpl/icon/avatar.jpg" width="50" height="50" alt="avatar user" /></a></li>
-                        </ul>
-                    </div>
-                    <!-- USER END -->
-                </div>
-            </header>
-            <!-- HEADER END -->	
-            <!-- CONTENT START -->
-            <section>
-                <?php echo $content; ?>
-
-                <!-- FOOTER START -->
-                <div class="warp l">
-                    <div class="footer">
-                        <ul>
-                            <li><a href="#" title="Movie Bay Plus">Movie Bay Plus</a></li>                         
-                            <li><a href="#" title="Blog">Blog</a></li>
-                            <li><a href="#" title="Advertising">Advertising</a></li>
-                            <li><a href="#" title="Jobs">Jobs</a></li>
-                            <li><a href="#" title="Help">Help</a></li>
-                            <li><a href="#" title="About Us">About Us</a></li>
-                        </ul>
-                        <ul class="term">
-                            <li>Privacy Policy &copy; 2013 Movie Bay</li> 
-                            <li><a href="#" title="term of Use">Terms of Use</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- FOOTER END -->
-            </section>	
-            <!-- CONTENT END -->	
-        </div>
+					<!-- USER START -->
+					<div class="user l">
+						<ul>
+							<li><a href="#" title="Upgrade previllage">UPGRADE</a></li>
+							<li><a href="#" title="Nama USer">Si Fulan (0)</a>
+								<ul>
+									<li><a href="#" title="Akun">Akun</a></li>
+									<li><a href="#" title="Pesan">Pesan <span class="r">(0)</span></a></li>
+									<li><a href="#" title="favorit">Favorit</a></li>
+									<li><a href="#" title="Petunjuk">Petunjuk</a></li>
+									<li><a href="#" title="Keluar">Keluar</a></li>
+								</ul>
+							</li>
+							<li><a href="#" title="Avatar"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/tpl/icon/avatar.jpg" width="50" height="50" alt="avatar user" /></a></li>
+						</ul>
+					</div>
+					<!-- USER END -->
+				</div>
+			</div>
+		</div>
+		<!-- HEADER END -->	
+			<div class="l bc">
+				<div class="container">
+					<?php if (isset($this->breadcrumbs)): ?>
+					<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+						'links'=>$this->breadcrumbs,
+					)); ?><!-- breadcrumbs -->
+					<?php endif ?>
+				</div>
+			</div>
+		<!-- CONTENT START -->
+		<div class="section">
+			<div class="container">
+				<?php echo $content; ?>
+			</div>	
+		</div>	
+		<!-- CONTENT END -->
+		<!-- FOOTER START -->
+		<div class="warp l">
+			<div class="container">
+				<div class="footer">
+					<ul>
+						<li><a href="#" title="Movie Bay Plus">Movie Bay Plus</a></li>                         
+						<li><a href="#" title="Blog">Blog</a></li>
+						<li><a href="#" title="Advertising">Advertising</a></li>
+						<li><a href="#" title="Jobs">Jobs</a></li>
+						<li><a href="#" title="Help">Help</a></li>
+						<li><a href="#" title="About Us">About Us</a></li>
+					</ul>
+					<ul class="term">
+						<li>Privacy Policy &copy; 2013 Movie Bay</li> 
+						<li><a href="#" title="term of Use">Terms of Use</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- FOOTER END -->
     </body>
 </html>
